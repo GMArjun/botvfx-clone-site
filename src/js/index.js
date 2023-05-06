7d5a9e037ca238ae1549abafb4b4a1b78531d4f2
Arjun Pandi <gmarjun100@gmail.com>
2023-05-06 10:50:04 +0530

    added loader for page

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,14 @@ import sal from "sal.js";
 import "./sliders";
 import "../scss/style.scss";
 
+// Show Loader Gracefully on Page Load
+document.body.style.overflow = "hidden";
+window.addEventListener("load", function () {
+  var loader = document.getElementById("loading-indicator");
+  loader.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
 sal({
   threshold: 1,
   once: true,
